1e6e33f7daa31b12be9cf32bf7926f2496f53184
Ted Oiler <ladas.thodoris@gmail.com>
2023-02-23 23:34:50 +0000

    delete columns funcitonality

--- nomobot_test.py
+++ nomobot_test.py
@@ -21,7 +21,7 @@ def run_selenium_cycle(data: pd.DataFrame) -> pd.DataFrame:
 
     browser.get("https://extapps.solon.gov.gr/mojwp/faces/TrackLdoPublic")
     data_out = []
-    for _, row in data.iterrows():
+    for iteration, row in data.iterrows():
         court = browser.find_element(By.XPATH, '//*[@id="courtOfficeOC::content"]')
         # court.send_keys(Keys.COMMAND + 'a' + Keys.DELETE)
         court.send_keys(Keys.CONTROL + 'a' + Keys.DELETE)
@@ -39,27 +39,36 @@ def run_selenium_cycle(data: pd.DataFrame) -> pd.DataFrame:
         browser.find_element(By.XPATH, '//*[@id="ldoSearch"]/a/span').click()
         time.sleep(2)
         selector = '#pc1\:ldoTable\:\:db > table > tbody > tr > td:nth-child(2) > div > table > tbody > tr > td:nth-child(1) > span'
-        gak_year = str(row['GAK'])+'/'+str(row['Year'])
-        WebDriverWait(browser, 20).until(lambda browser: browser.find_element(By.CSS_SELECTOR, selector).text == gak_year)
+        gak_year = str(row['GAK']) + '/' + str(row['Year'])
+        WebDriverWait(browser, 30).until(
+            lambda browser: browser.find_element(By.CSS_SELECTOR, selector).text == gak_year)
 
-        condition = True
-        while condition:
-            try:
-                scraper_number_selector = '#pc1\:ldoTable\:\:db > table > tbody > tr > td:nth-child(2) > div > table > tbody > tr > td:nth-child(1)'
-                scraped_number = browser.find_element(By.CSS_SELECTOR, scraper_number_selector).text
+        table_code = browser.find_element(By.CSS_SELECTOR, '#pb2\:\:content')
+        scraped_number = browser.find_element(By.CSS_SELECTOR,
+                                              '#pc1\:ldoTable\:\:db > table > tbody > tr > td:nth-child(2) > div > table > tbody > tr > td:nth-child(1)').text
+        time.sleep(1)
+
+        if iteration == 0:
+            columns_xpaths_list = ['//*[@id="pc1:_shwClmc11"]/td[2]',
+                                   '//*[@id="pc1:_shwClmc9"]/td[2]',
+                                   '//*[@id="pc1:_shwClmc6"]/td[2]',
+                                   '//*[@id="pc1:_shwClmc7"]/td[2]']
+
+            for col in columns_xpaths_list:
+                browser.find_element(By.XPATH, '//*[@id="pc1:_vw"]/div/table/tbody/tr/td[2]/a').click()
                 time.sleep(1)
+                browser.find_element(By.XPATH, '//*[@id="pc1:_clmns"]/td[2]').click()
+                time.sleep(1)
+                WebDriverWait(browser, 30).until(
+                    lambda browser: browser.find_element(By.CSS_SELECTOR, selector).text == gak_year)
+                browser.find_element(By.XPATH, col).click()
+                time.sleep(5)
 
-                results_selector = '#pc1\:ldoTable\:\:db > table > tbody > tr > td:nth-child(2) > div > table > tbody > tr > td:nth-child(7)'
-                element = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, results_selector)))
-                browser.execute_script("return arguments[0].scrollIntoView(true);", element)
-                result = browser.find_element(By.CSS_SELECTOR, results_selector).text
-                condition = False
-            except:
-                time.sleep(10)
+        time.sleep(3)
+        result = browser.find_element(By.CSS_SELECTOR,
+                                      '#pc1\:ldoTable\:\:db > table > tbody > tr > td:nth-child(2) > div > table > tbody > tr > td:nth-child(4)').text
 
         data_out.append([row['Court'], row['GAK'], row['Year'], scraped_number, result, date.today()])
-        # browser.execute_script("location.reload()")
-        # browser.get("https://extapps.solon.gov.gr/mojwp/faces/TrackLdoPublic")
     data_out_df = pd.DataFrame(data_out, columns=['Court', 'GAK', 'Year', 'Scraped_GAK', 'Result', 'Date'])
     # data_out_df.to_csv('./data/data_out.csv', index=False)
     time.sleep(1)

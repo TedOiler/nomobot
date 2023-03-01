6ddb177e3a0a179db2dcdb3f3c90fbed525e06be
Ted Oiler <ladas.thodoris@gmail.com>
2023-03-01 00:08:14 +0000

    changed server to streamlit

--- nomobot_test.py
+++ nomobot_test.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import time
@@ -10,12 +13,16 @@ import pandas as pd
 
 
 def run_selenium_cycle(data: pd.DataFrame) -> pd.DataFrame:
-    op = webdriver.ChromeOptions()
-    op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    def get_driver():
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=op)
+
+    op = Options()
+    # op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     op.add_argument("--headless")
     op.add_argument("--no-sandbox")
     op.add_argument("--disable-dev-sh-usage")
-    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+    browser = get_driver()
+    # browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
     # browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     browser.get("https://extapps.solon.gov.gr/mojwp/faces/TrackLdoPublic")
